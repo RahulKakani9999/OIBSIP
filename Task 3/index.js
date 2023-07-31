755107b7f2b44c9834b70661cf509dab80f1a2c6
Rahul Chowdary Kakani <95332091+RahulKakani9999@users.noreply.github.com>
2023-07-31 14:00:18 +0530

    Update index.js

--- Task 3/index.js
+++ Task 3/index.js
@@ -1,62 +1,62 @@
 window.addEventListener('load', () => {
-	const form = document.querySelector("#task-form");
-	const input = document.querySelector("#task-input");
-	const list_element = document.querySelector("#tasks");
+	const form = document.querySelector("#tf");
+	const input = document.querySelector("#ti");
+	const le = document.querySelector("#tasks");
 
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
 
-		const task = input.value;
+		const tk = input.value;
 
-		const task_element = document.createElement('div');
-		task_element.classList.add('task');
+		const te = document.createElement('div');
+		te.classList.add('task');
 
-		const task_content_element = document.createElement('div');
-		task_content_element.classList.add('content');
+		const tce = document.createElement('div');
+		tce.classList.add('content');
 
-		task_element.appendChild(task_content_element);
+		te.appendChild(tce);
 
-		const task_input_element = document.createElement('input');
-		task_input_element.classList.add('text');
-		task_input_element.type = 'text';
-		task_input_element.value = task;
-		task_input_element.setAttribute('readonly', 'readonly');
+		const tie = document.createElement('input');
+		tie.classList.add('text');
+		tie.type = 'text';
+		tie.value = tk;
+		tie.setAttribute('readonly', 'readonly');
 
-		task_content_element.appendChild(task_input_element);
+		tce.appendChild(tie);
 
-		const task_actions_element = document.createElement('div');
-		task_actions_element.classList.add('actions');
+		const tae = document.createElement('div');
+		tae.classList.add('actions');
 		
-		const task_edit_element = document.createElement('button');
-		task_edit_element.classList.add('edit');
-		task_edit_element.innerText = 'Edit';
+		const taee = document.createElement('button');
+		taee.classList.add('edit');
+		taee.innerText = 'Edit';
 
-		const task_delete_element = document.createElement('button');
-		task_delete_element.classList.add('delete');
-		task_delete_element.innerText = 'Delete';
+		const tade = document.createElement('button');
+		tade.classList.add('delete');
+		tade.innerText = 'Delete';
 
-		task_actions_element.appendChild(task_edit_element);
-		task_actions_element.appendChild(task_delete_element);
+		tae.appendChild(taee);
+		tae.appendChild(tade);
 
-		task_element.appendChild(task_actions_element);
+		te.appendChild(tae);
 
-		list_element.appendChild(task_element);
+		le.appendChild(te);
 
 		input.value = '';
 
-		task_edit_element.addEventListener('click', (e) => {
-			if (task_edit_element.innerText.toLowerCase() == "edit") {
-				task_edit_element.innerText = "Save";
-				task_input_element.removeAttribute("readonly");
-				task_input_element.focus();
+		taee.addEventListener('click', (e) => {
+			if (taee.innerText.toLowerCase() == "edit") {
+				taee.innerText = "Save";
+				tie.removeAttribute("readonly");
+				tie.focus();
 			} else {
-				task_edit_element.innerText = "Edit";
-				task_input_element.setAttribute("readonly", "readonly");
+				taee.innerText = "Edit";
+				tie.setAttribute("readonly", "readonly");
 			}
 		});
 
-		task_delete_element.addEventListener('click', (e) => {
-			list_element.removeChild(task_element);
+		tade.addEventListener('click', (e) => {
+			le.removeChild(te);
 		});
 	});
 });
